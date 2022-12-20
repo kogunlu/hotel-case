222efaff095d00f5ea40d15bb472a7731f52a2e4
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-12-20 13:33:57 +0300

    Navbar | hover effect improvement

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,16 +14,16 @@ function Navbar() {
     <nav className='h-1/6 w-full border-solid border-none flex flex-col align-center'>
       <div className='flex flex-col items-center justify-center bg-black pb-2'>
         <Logo />
-        <p className='text-white text-xl text-center font-serif font-bold pt-2'>YOUR SECOND HOME</p>
+        <p className='text-white text-xl text-center font-serif font-bold pt-2 hover:text-red-600 hover:underline hover:underline-offset-4'>YOUR SECOND HOME</p>
 
       </div>
 
       <div className='flex justify-center bg-black'>
       <div className='w-full md:w-10/12 lg:text-xl lg:w-6/12 h-3/6 flex justify-around pt-1 pb-1 bg-black text-white'>
-        <Link to={"/"} className="border-transparent border-b-2 hover:border-red-900 hover:text-red-600">Anasayfa</Link>
-        <Link className="border-transparent border-b-2 hover:border-red-900	 hover:text-red-600" to={"/rooms"}>Odalar</Link>
-        <Link className="border-transparent border-b-2 hover:border-red-900 hover:text-red-600" to={"/gallery"}>Galeri</Link>
-        <Link className="border-transparent border-b-2 hover:border-red-900 hover:text-red-600" to={"/contact"}>İletişim</Link>
+        <Link to={"/"} className="border-transparent border-b-2 hover:border-red-900 hover:text-red-600 hover:text-2xl">Anasayfa</Link>
+        <Link className="border-transparent border-b-2 hover:border-red-900	 hover:text-red-600 hover:text-2xl" to={"/rooms"}>Odalar</Link>
+        <Link className="border-transparent border-b-2 hover:border-red-900 hover:text-red-600 hover:text-2xl" to={"/gallery"}>Galeri</Link>
+        <Link className="border-transparent border-b-2 hover:border-red-900 hover:text-red-600 hover:text-2xl" to={"/contact"}>İletişim</Link>
       </div>
       </div>
       
